4f14c9440700bec70b7b8ef0d35028312ee97f55
Viviana Ortiz Tellez <viviortiz1418@gmail.com>
2025-03-18 16:16:33 -0600

    Crear función para agregar amigos a la lista

--- app.js
+++ app.js
@@ -2,3 +2,20 @@
 // Lista para almacenar los nombres de los amigos
 let amigos = [];
 
+// Función para añadir un nombre a la lista de amigos
+function agregarAmigo() {
+    const nombreAmigo = document.getElementById("amigo").value;
+
+    // Comprobamos que el campo no esté vacío
+    if (nombreAmigo.trim() !== "") {
+        // Añadir el nombre al arreglo de amigos
+        amigos.push(nombreAmigo);
+
+    // Limpiar el campo de entrada para el siguiente nombre
+    document.getElementById("amigo").value = "";
+
+    mostrarAmigos();
+    } else {
+        alert("Por favor, ingresa un nombre válido.");
+    }
+}
